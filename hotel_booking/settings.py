22f8eae74899af29b1e8b63fbeb92372cfda71a1
Shehab Uddin <shehabuddin380@gmail.com>
2026-03-04 00:22:13 +0600

    update setting

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -5,24 +5,21 @@ import cloudinary
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Security
-
 SECRET_KEY = config("SECRET_KEY", default="insecure-secret-key")
-DEBUG = config("DEBUG", default=True, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
-    "ALLOWED_HOSTS", 
-    default="127.0.0.1,localhost,hotel-booking-website-pied.vercel.app", 
-    cast=Csv()
+    "ALLOWED_HOSTS",
+    default="127.0.0.1,localhost,.vercel.app",
+    cast=Csv(),
 )
 
 AUTH_USER_MODEL = "users.User"
 
 
-# Installed Apps
-
 INSTALLED_APPS = [
     "whitenoise.runserver_nostatic",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -33,11 +30,8 @@ INSTALLED_APPS = [
     # Third-party
     'rest_framework',
     'rest_framework_simplejwt',
-    'drf_yasg',
-    'debug_toolbar',
     'cloudinary',
     'cloudinary_storage',
-    'corsheaders',
 
     # Local apps
     'api',
@@ -47,19 +41,17 @@ INSTALLED_APPS = [
     'payments',
 ]
 
-# Middleware
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'hotel_booking.urls'
@@ -81,47 +73,32 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'hotel_booking.wsgi.application'
 
-# Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME'),
-#         'USER': config('DB_USER'),
-#         'PASSWORD': config('DB_PASSWORD'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT', default=5432, cast=int),
-#     }
-# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3", 
-                }
+        'NAME': BASE_DIR / "db.sqlite3",
+    }
 }
 
-
-
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 
 cloudinary.config(
-    cloud_name=config('CLOUDINARY_CLOUD_NAME',default=""),
-    api_key=config('CLOUDINARY_API_KEY',default=""),
-    api_secret=config('CLOUDINARY_API_SECRET',default=""),
+    cloud_name=config('CLOUDINARY_CLOUD_NAME', default=""),
+    api_key=config('CLOUDINARY_API_KEY', default=""),
+    api_secret=config('CLOUDINARY_API_SECRET', default=""),
     secure=True
 )
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
 
 
 STATIC_URL = '/static/'
@@ -131,18 +108,23 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
-INTERNAL_IPS = ["127.0.0.1"]
-
-
-# Email (SMTP)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER",default="")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD",default="")
-DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="Hotel Booking <noreply@hotel.com>")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = config(
+    "DEFAULT_FROM_EMAIL",
+    default="Hotel Booking <noreply@hotel.com>"
+)
+
+
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

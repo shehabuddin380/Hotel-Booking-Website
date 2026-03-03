b64cbc107d127788df623b4cd5459edde42e916a
Shehab Uddin <shehabuddin380@gmail.com>
2026-03-03 17:37:33 +0600

    add cloudingnary env

--- users/models.py
+++ users/models.py
@@ -4,16 +4,23 @@ from users.managers import CustomUserManager
 
 
 class User(AbstractUser):
-    username = None  
+
+    username = None
+
     email = models.EmailField(unique=True)
     is_admin = models.BooleanField(default=False)
-    # Extra fields
+
     address = models.TextField(blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
+
+    balance = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        default=0.00
+    )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []  
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 

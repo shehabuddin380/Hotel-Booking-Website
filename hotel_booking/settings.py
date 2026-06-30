35ada629697e551de26007c1988dec30b6e52bbb
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-01 02:22:20 +0600

    testing commit

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -82,12 +82,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'hotel_booking.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 DATABASES = {

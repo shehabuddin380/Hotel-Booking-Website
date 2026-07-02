612d9d8ab79c196afd1b421ccc2ed3e1835257f9
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-03 04:49:38 +0600

    colose media root

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -129,7 +129,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = BASE_DIR / "media"
+# MEDIA_ROOT = BASE_DIR / "media"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

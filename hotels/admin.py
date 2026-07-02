73ffd293e916cc64ed96c5ad60b46f694006db67
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-02 22:39:39 +0600

    update admin.py and fix

--- hotels/admin.py
+++ hotels/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hotel, Room, Booking, Review, Order
 
-# Register your models here.
+admin.site.register(Hotel)
+admin.site.register(Room)
+admin.site.register(Booking)
+admin.site.register(Review)
+admin.site.register(Order)

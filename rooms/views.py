fd3836ff12f52edcc1a627ee43e122152d71cbf5
Shihab Ahmed <shehabuddin380@gmail.com>
2026-07-04 19:55:02 +0600

    update views.py

--- rooms/views.py
+++ rooms/views.py
@@ -4,3 +4,8 @@ from rest_framework import status
 from .models import Room
 from .serializers import RoomSerializer
 
+class RoomListAPIView(APIView):
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
